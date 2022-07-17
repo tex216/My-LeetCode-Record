7be1ca6a063c1e634e06dcdb96d66e96e053911a
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-16 23:15:52 -0400

    Time: 2 ms (99.28%), Space: 42.2 MB (93.42%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode currNode = dummy;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            currNode.next = new ListNode(sum % 10);
+            currNode = currNode.next;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        return dummy.next;
+    }
+}
