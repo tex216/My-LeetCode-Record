be04a8d9a68a27cc7f8d6d60e69a13167020de6c
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-16 23:20:21 -0400

    Time: 0 ms (100.00%), Space: 44.1 MB (12.17%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] < target) {
+                left = middle + 1;
+            }
+            else if (nums[middle] > target) {
+                right = middle - 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return left;
+    }
+}
