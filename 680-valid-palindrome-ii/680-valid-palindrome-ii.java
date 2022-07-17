8359f49c63647d0bdadbe7e021c8018aef21d61e
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-16 23:36:12 -0400

    Time: 12 ms (46.97%), Space: 55 MB (22.58%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return checkPalindrome(s, left, right-1) ||                                      checkPalindrome(s, left+1, right);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    private boolean checkPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
