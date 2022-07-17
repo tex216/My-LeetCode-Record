3e37b7e2830ed5385ffff3d81fa3d658e5791e7c
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-16 23:30:41 -0400

    Time: 55 ms (27.98%), Space: 54.8 MB (7.46%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        //store each character's frequency
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        //find the index
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.get(c) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    
+    }
+}
