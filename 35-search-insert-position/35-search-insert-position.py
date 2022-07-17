3c302187f2f09715ed6b562517d94cb0d5c9cd4f
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-16 23:20:30 -0400

    Time: 55 ms (40.11%), Space: 13.9 MB (94.84%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)-1
+        while left <= right :
+            middle = (left + right) // 2
+            if nums[middle] < target:
+                left = middle + 1
+            elif nums[middle] > target:
+                right = middle - 1
+            else:
+                return middle
+        return left
+        
