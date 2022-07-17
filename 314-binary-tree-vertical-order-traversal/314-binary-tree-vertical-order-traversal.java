68997001581c97e60a7c6abba956b000f839778b
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-16 23:29:13 -0400

    Time: 5 ms (52.72%), Space: 43.7 MB (39.80%) - LeetHub

---
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList();
+        if (root == null) return ans;
+        
+        Map<Integer, ArrayList> columnTable = new HashMap();
+        Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque();
+        int column = 0, minColumn = 0, maxColumn = 0;
+        queue.offer(new Pair(root, column));
+        
+        while (!queue.isEmpty()) {
+          Pair<TreeNode, Integer> p = queue.poll();
+          root = p.getKey();
+          column = p.getValue();
+
+          if (root != null) {
+            if (!columnTable.containsKey(column)) {
+              columnTable.put(column, new ArrayList<Integer>());
+            }
+            columnTable.get(column).add(root.val);
+            minColumn = Math.min(minColumn, column);
+            maxColumn = Math.max(maxColumn, column);
+
+            queue.offer(new Pair(root.left, column - 1));
+            queue.offer(new Pair(root.right, column + 1));
+          }
+        }
+        
+        for (int i = minColumn; i < maxColumn + 1; i++) {
+            ans.add(columnTable.get(i));
+        }
+        
+        return ans;    
+        
+    }
+}
