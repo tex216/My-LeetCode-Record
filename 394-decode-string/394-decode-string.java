88a80a33f952d68d32a34c3635860ba25a9be40d
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-16 23:31:02 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (80.20%) - LeetHub

---
+++ 394-decode-string/394-decode-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    int index = 0;
+    public String decodeString(String s) {
+        //The recursion uses an internal call stack to store the previous state
+        StringBuilder result = new StringBuilder();
+        while (index < s.length() && s.charAt(index) != ']') {
+            if (!Character.isDigit(s.charAt(index)))
+                result.append(s.charAt(index++));
+            else {
+                int k = 0;
+                // build k while next character is a digit
+                while (index < s.length() &&                                                    Character.isDigit(s.charAt(index)))
+                    k = k * 10 + s.charAt(index++) - '0';
+                // ignore the opening bracket '['    
+                index++;
+                String decodedString = decodeString(s);
+                // ignore the closing bracket ']'
+                index++;
+                // build k[decodedString] and append to the result
+                while (k-- > 0)
+                    result.append(decodedString);
+            }
+        }
+        return result.toString();
+    }
+}
