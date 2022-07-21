da19b22a92bb214f8628d14936fe924900736969
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-21 00:59:29 -0400

    Time: 1404 ms (5.39%), Space: 181.4 MB (5.00%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> ans = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[left], nums[right]);
+                    ans.add(temp);
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }  
+}
