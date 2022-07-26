78e864b01e13eba6bd83f57c434ffec90c245b38
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-26 01:17:35 -0400

    Time: 7 ms (96.01%), Space: 44.7 MB (99.80%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,7 +1,9 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
+        
         for (String word : strs) {
+            //string is immutable, to sort word, it has to be converted to charArray then convert back
             char[] charArray = word.toCharArray();
             Arrays.sort(charArray);
             String sortedWord = new String(charArray);
