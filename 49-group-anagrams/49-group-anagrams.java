5b9b7ccd4e094f1854374ef88247756a17bf6f48
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-07-26 01:15:47 -0400

    Time: 12 ms (69.02%), Space: 56.4 MB (39.12%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] charArray = word.toCharArray();
+            Arrays.sort(charArray);
+            String sortedWord = new String(charArray);
+            
+            if (map.containsKey(sortedWord)) {
+                map.get(sortedWord).add(word);
+            }
+            else {
+                map.put(sortedWord, new ArrayList<String>                                                               (Arrays.asList(word)));
+            }
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
