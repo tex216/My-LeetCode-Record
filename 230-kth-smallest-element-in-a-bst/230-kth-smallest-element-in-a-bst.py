f503b56d092341b124031f04dc983031f070596c
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-06 22:47:07 -0400

    Time: 66 ms (45.25%), Space: 21.1 MB (81.72%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -11,17 +11,13 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = 1
         stack = []
-        node = root
-        while stack or node:
-            if node:
-                stack.append(node)
-                node = node.left
-            else:
-                node = stack.pop()
-                if count == k:
-                    return node.val
-                count += 1
-                node = node.right
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0: return root.val
+            root = root.right
                 
