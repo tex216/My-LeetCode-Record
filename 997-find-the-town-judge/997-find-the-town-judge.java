b7cccd0ee2bdfbcbd4400849f7ae419a7d110713
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-13 16:29:56 -0400

    Time: 2 ms (99.91%), Space: 74.2 MB (25.81%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -2,18 +2,16 @@ class Solution {
     public int findJudge(int n, int[][] trust) {
         if (trust.length < n-1) return -1;
         
-        int[] indegree = new int[n+1];
-        int[] outdegree = new int[n+1];
+        int[] score = new int[n+1];
         
         for (int[] group : trust) {
-            outdegree[group[0]]++;
-            indegree[group[1]]++;
+            score[group[0]]--;
+            score[group[1]]++;
         }
         
         for (int i = 1; i <= n; i++) {
-            if (indegree[i] == n-1 && outdegree[i] == 0) return i;
+            if (score[i] == n-1) return i;
         }
-        
         return -1;
     }
 }
