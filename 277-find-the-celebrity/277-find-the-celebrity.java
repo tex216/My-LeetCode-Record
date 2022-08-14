13b875a4148e9918273ca03035cfde1a9f7dcd41
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-14 00:26:22 -0400

    Time: 24 ms (68.00%), Space: 54.5 MB (16.84%) - LeetHub

---
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -0,0 +1,23 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    
+    public int findCelebrity(int n) {
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(ans, i)) ans = i;
+        }
+        
+        if (isCelebrity(n, ans)) return ans;
+        return -1;
+    }
+    
+    private boolean isCelebrity(int n, int ans) {
+        for (int i = 0; i < n; i++) {
+            if (i == ans) continue;
+            if (knows(ans, i) || !knows(i, ans)) return false;
+        }
+        return true;
+    }
+}
