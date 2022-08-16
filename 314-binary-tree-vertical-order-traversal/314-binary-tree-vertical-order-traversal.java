af4cbdc473b2d8178ee3929b174543910dfe0b16
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-15 22:48:26 -0400

    Time: 4 ms (74.52%), Space: 43.9 MB (16.18%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
@@ -15,37 +15,36 @@
  */
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList();
-        if (root == null) return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;   
+        Map<Integer, ArrayList> map = new HashMap<>();
+        Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque<>();
         
-        Map<Integer, ArrayList> columnTable = new HashMap();
-        Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque();
-        int column = 0, minColumn = 0, maxColumn = 0;
-        queue.offer(new Pair(root, column));
+        int column = 0, minCol = 0, maxCol = 0;
+        queue.add(new Pair(root, column));
         
-        while (!queue.isEmpty()) {
-          Pair<TreeNode, Integer> p = queue.poll();
-          root = p.getKey();
-          column = p.getValue();
-
-          if (root != null) {
-            if (!columnTable.containsKey(column)) {
-              columnTable.put(column, new ArrayList<Integer>());
-            }
-            columnTable.get(column).add(root.val);
-            minColumn = Math.min(minColumn, column);
-            maxColumn = Math.max(maxColumn, column);
-
-            queue.offer(new Pair(root.left, column - 1));
-            queue.offer(new Pair(root.right, column + 1));
-          }
+        //BFS
+        while (queue.size() > 0) {
+            Pair<TreeNode, Integer> p = queue.poll();
+            root = p.getKey();
+            column = p.getValue();
+            
+            if (root != null) {
+                if (!map.containsKey(column)) {
+                    map.put(column, new ArrayList<Integer>());
+                }
+                map.get(column).add(root.val);
+                minCol = Math.min(minCol, column);
+                maxCol = Math.max(maxCol, column);
+                queue.add(new Pair(root.left, column-1));
+                queue.add(new Pair(root.right, column+1));
+            }    
         }
-        
-        for (int i = minColumn; i < maxColumn + 1; i++) {
-            ans.add(columnTable.get(i));
+        //Avoid sorting by iterating through the range of the column index, this assumes no missing column index (no any broken branch in the tree) 
+        for (int i = minCol; i <= maxCol; i++) {
+            ans.add(map.get(i));
         }
-        
-        return ans;    
-        
+        return ans;
     }
 }
+     
