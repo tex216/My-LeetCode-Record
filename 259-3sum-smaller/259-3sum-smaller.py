9236ba93886e94ea82f1f30c59abf83223a4f5e5
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-17 23:19:29 -0400

    Time: 971 ms (32.35%), Space: 13.7 MB (19.61%) - LeetHub

---
+++ 259-3sum-smaller/259-3sum-smaller.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def threeSumSmaller(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        count = 0
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                if nums[i] + nums[left] + nums[right] < target:
+                    count += right - left
+                    left += 1
+                else:
+                    right -= 1
+        return count
