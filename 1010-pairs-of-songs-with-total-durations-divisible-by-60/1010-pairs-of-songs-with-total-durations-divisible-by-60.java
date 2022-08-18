3aef404a81d90d52db7074e9f93a9382135ebdf4
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-18 16:55:14 -0400

    Time: 3 ms (94.87%), Space: 49.3 MB (92.70%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        int remainders[] = new int[60];
+        int[] remainders = new int[60];
         int count = 0;
         for (int t : time) {
             //we want to know for each t, how many x satisfy (t + x) % 60 = 0. Aka. x % 60 = 60 - t % 60. But if t % 60 = 0, x % 60 = 0 instead of 60.
