720e4a64c4720e8c89d64a820d67afb78b390106
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-24 23:22:03 -0400

    Time: 0 ms (100.00%), Space: 56 MB (5.41%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,13 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-            if (map.containsKey(nums[i])) {
-                if (nums[i] == target) {
-                    return map.get(nums[i]);
-                }
-            } 
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] < target) left = middle + 1;
+            else if (nums[middle] > target) right = middle - 1;
+            else return middle;
         }
         return -1;
     }
