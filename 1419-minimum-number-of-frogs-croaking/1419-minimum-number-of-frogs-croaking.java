b34fc267d69289766dfdc6bccfac45ec510b057c
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-08-29 23:32:36 -0400

    Time: 18 ms (46.39%), Space: 47.1 MB (49.22%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.java
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.java
@@ -5,17 +5,16 @@ class Solution {
         int num = 0, ans = 0;
         for (char c : s.toCharArray()) {
             int idx = croak.indexOf(c); //0-4
-            if (idx > 0 && count[idx-1] == 0) return -1;
+            if (idx > 0 && count[idx-1] == 0) return -1;// invalid: another frog is croaking with no prev
             //for each c
             if (idx > 0) count[idx-1]--;
             count[idx]++;
             
-            if (idx == 0) num++;
-            
-            if (idx == 4) num--;
-            
+            if (idx == 0) num++; // 'c'
+            if (idx == 4) num--; // 'k'
             ans = Math.max(ans, num);
         }
+        //check if any frog hasn't finished croaking
         return (count[0] + count[1] + count[2] + count[3] > 0) ? -1 : ans;
       
     }
