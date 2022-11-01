8bbfe5d468188e4c7e04704360720d1bae70aa7b
Teng (Elijah)  Xue <tex216@lehigh.edu>
2022-10-31 21:01:40 -0400

    Time: 132 ms (29.59%), Space: 16.6 MB (73.66%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        intervals.sort()
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+        return True
